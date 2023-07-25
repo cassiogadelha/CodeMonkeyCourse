9544e83ec8999957fb399d3399434d1fc6635e14
Cássio Gadêlha <cassiogadelha15@gmail.com>
2023-07-25 14:26:46 -0300

    PlayerCollision
    Criação de um CapsuleRayCast para detectar colisões

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -13,7 +13,37 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
-        transform.position += moveSpeed * Time.deltaTime * moveDir;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            
+            if(canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if(canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        { 
+            transform.position += moveDistance * moveDir;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
