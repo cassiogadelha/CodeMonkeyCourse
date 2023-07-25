470ba72f7bf65f7f02f811a25f38b18a962f1bea
Cássio Gadêlha <cassiogadelha15@gmail.com>
2023-07-25 11:02:05 -0300

    Animator
    Adicionado a animação de caminhada do Player

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new(0, 0);
@@ -31,7 +33,14 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
         transform.position += moveSpeed * Time.deltaTime * moveDir;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
