e041079a8b6bdba6176d235f674cf685b253fff7
Cássio Gadêlha <cassiogadelha15@gmail.com>
2023-07-27 14:50:52 -0300

    CuttingCounter
    Adição da bancada de corte

--- Game/Assets/Scripts/BaseCounter.cs
+++ Game/Assets/Scripts/BaseCounter.cs
@@ -9,6 +9,7 @@ public class BaseCounter : MonoBehaviour, IKicthenObjectParent
     private KitchenObject kitchenObject;
 
     public virtual void Interact(Player player) { }
+    public virtual void InteractAlternate(Player player) { }
     public Transform GetKitchenObjectFollowTransform()
     {
         return counterTopPoint;
