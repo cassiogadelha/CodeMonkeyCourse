7dd40d295883d0ef0c1f1c437e12958068220a12
Cássio Gadêlha <cassiogadelha15@gmail.com>
2023-07-30 16:38:05 -0300

    PlatePickUpObjects

--- Game/Assets/Scripts/CuttingCounter.cs
+++ Game/Assets/Scripts/CuttingCounter.cs
@@ -39,7 +39,11 @@ public class CuttingCounter : BaseCounter, IHasProgress
         {
             if (player.HasKitchenObject())
             {
-
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                        GetKitchenObject().DestroySelf();
+                }
             }
             else
             {
