cabeacf6e4013345091f9a21416678cefa94b1de
Cássio Gadêlha <cassiogadelha15@gmail.com>
2023-07-30 22:47:02 -0300

    PlateWorldUiIcons

--- Game/Assets/Scripts/CuttingCounter.cs
+++ Game/Assets/Scripts/CuttingCounter.cs
@@ -32,7 +32,7 @@ public class CuttingCounter : BaseCounter, IHasProgress
             }
             else
             {
-
+    
             }
         }
         else
--- Game/Assets/Scripts/PlateKitchenObject.cs
+++ Game/Assets/Scripts/PlateKitchenObject.cs
@@ -37,4 +37,8 @@ public class PlateKitchenObject : KitchenObject
             return true;
         }
     }
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
 }
